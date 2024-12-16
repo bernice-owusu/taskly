2c206bfa349cb2ac2ced612a92476c0bcc4bdf67
bernice <bernyx.owusu@gmail.com>
2024-12-16 23:28:09 +0000

    taskly setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style ={{
+        borderBottomColor: "#1a",
+        paddingHorizontal: 8,
+        paddingVertical: 16,
+      }}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
 });
