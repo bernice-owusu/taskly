6d8b0d9388d058ef2b4bfb87252333945052e193
bernice <bernyx.owusu@gmail.com>
2024-12-17 15:19:35 +0000

    bottom tab icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,41 @@
 import { Tabs } from "expo-router";
-
+import { AntDesign, Feather, FontAwesome5 } from "@expo/vector-icons";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Tabs>
-        <Tabs.Screen name="index" options={{title: "Shoppinglist"}}/>
-        <Tabs.Screen name="counter" options={{title: "Counter"}}/>
-        <Tabs.Screen name="idea" options={{title: "Idea"}}/>
+    <Tabs screenOptions={{
+        tabBarActiveTintColor:theme.primaryColor,  
+        tabBarActiveBackgroundColor:"white",
+       }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shoppinglist",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="shopping-bag" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircleo" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
-  )
+  );
 }
