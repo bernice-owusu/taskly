6ca0417949c4e8e52fde7a1d6b7bbc8f1703ba89
bernice <bernyx.owusu@gmail.com>
2025-01-03 15:46:53 +0000

    counter

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,41 +1,141 @@
 import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
-// import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 import { theme } from "../../theme";
 import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
+import { intervalToDuration, isBefore } from "date-fns";
+import { TimeSegment } from "../../components/TimeSegment";
+import { getFromStorage, saveToStorage } from "../../utils/storage";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
+import { isDevice } from "expo-device";
+
+const frequency = 10 * 2000;
+const countdownStorageKey = "taskly-countdown";
+
+type PersistedCountdownState = {
+  currentNotificationId: string | undefined;
+  completedAtTimestamps: number[];
+};
+
+type CountdownStatus = {
+  isOverdue: boolean;
+  distance: ReturnType<typeof intervalToDuration>;
+};
 
 export default function CounterScreen() {
+  const [countdownState, setCountdownState] =
+    useState<PersistedCountdownState>();
+  const [status, setStatus] = useState<CountdownStatus>({
+    isOverdue: false,
+    distance: {},
+  });
+
+  useEffect(() => {
+    const init = async () => {
+      const value = await getFromStorage(countdownStorageKey);
+      setCountdownState(value);
+    };
+    init();
+  }, []);
+
+  const lastCompletedAt = countdownState?.completedAtTimestamps[0];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const timestamp = lastCompletedAt
+        ? lastCompletedAt + frequency
+        : Date.now();
+
+      const isOverdue = isBefore(timestamp, Date.now());
+      const distance = intervalToDuration(
+        isOverdue
+          ? { start: timestamp, end: Date.now() }
+          : { start: Date.now(), end: timestamp }
+      );
+      setStatus({ isOverdue, distance });
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [lastCompletedAt]);
+
   const scheduleNotification = async () => {
-    // let result = await registerForPushNotificationsAsync();
-    Alert.alert("Successfull set scheduled notification");
-    // console.log(result);
-    // if (result === "granted") {
-    // await Notifications.scheduleNotificationAsync({
-    //   content: {
-    //     title: "I'm a notification from your app! 📨",
-    //     body: "This is a scheduled notification.",
-    //   },
-    //   trigger: {
-    //     type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
-    //     seconds: 5,
-    //     // repeats: true,
-    //   },
-    // });
-    // Alert.alert("Successfull set scheduled notification");
-    // } else {
-    // Alert.alert(
-    //   "Unable to schedule notification",
-    //   "Enable the notifications permission for Expo Go in settings"
-    // );
-    // }
+    let pushNotificationId;
+    const result = await registerForPushNotificationsAsync();
+    if (result === "granted") {
+      Alert.alert("You have a notification from Taskly!");
+      // pushNotificationId = await Notifications.scheduleNotificationAsync({
+      //   content: {
+      //     title: "Task is due!",
+      //   },
+      //   trigger: {
+      //     type: Notifications?.TriggerType.TIME_INTERVAL,
+      //     seconds: frequency / 1000,
+      //   },
+      // });
+    } else {
+      if (isDevice) {
+        Alert.alert("You need to enable notifications for this app");
+      }
+    }
+
+    if (countdownState?.currentNotificationId) {
+      await Notifications.cancelScheduledNotificationAsync(
+        countdownState.currentNotificationId
+      );
+    }
+
+    const newCountdownState: PersistedCountdownState = {
+      currentNotificationId: pushNotificationId,
+      completedAtTimestamps: countdownState
+        ? [Date.now(), ...countdownState.completedAtTimestamps]
+        : [Date.now()],
+    };
+
+    setCountdownState(newCountdownState);
+
+    await saveToStorage(countdownStorageKey, newCountdownState);
   };
+
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        status.isOverdue ? styles.containerLate : undefined,
+      ]}
+    >
+      {!status.isOverdue ? (
+        <Text style={[styles.heading]}>Thing due in</Text>
+      ) : (
+        <Text style={[styles.heading, styles.whiteText]}>Thing overdue by</Text>
+      )}
+      <View style={styles.row}>
+        <TimeSegment
+          unit="Days"
+          number={status.distance?.days ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Hours"
+          number={status.distance?.hours ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Minutes"
+          number={status.distance?.minutes ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Seconds"
+          number={status.distance?.seconds ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+      </View>
       <TouchableOpacity
         onPress={scheduleNotification}
         style={styles.button}
         activeOpacity={0.8}
       >
-        <Text style={styles.buttonText}>Schedule Permission</Text>
+        <Text style={styles.buttonText}>I've done the thing!</Text>
       </TouchableOpacity>
     </View>
   );
@@ -46,17 +146,35 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: theme.colorWhite,
   },
   button: {
     backgroundColor: theme.colorBlack,
     padding: 12,
     borderRadius: 6,
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
   buttonText: {
     color: "#fff",
     fontWeight: "bold",
     textTransform: "uppercase",
     letterSpacing: 1,
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 24,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 24,
+    color: theme.colorBlack,
+  },
+  containerLate: {
+    backgroundColor: theme.colorRed,
+  },
+  whiteText: {
+    color: theme.colorWhite,
   },
 });
